e5827ec11b2cc945d371a10298f01a1b2381c464
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-25 22:40:18 +0000

    Automatically backed up by Learn

---
+++ src/components/Band.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Band extends Component {
+
+  render() {
+    return(
+      <li>
+        {this.props.name}
+        <button onClick={() => this.props.delete(this.props.id)}>Delete</button>
+      </li>
+    );
+  }
+};
+
+export default Band;
